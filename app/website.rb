c222fbd4e1f3a3f6b1acaf83589eb7dd93ab1273
Néville Dubuis <ndubuis@gmail.com>
2014-06-09 17:18:26 +0200

    finalize website

--- app/website.rb
+++ app/website.rb
@@ -2,9 +2,26 @@
 require 'sinatra/base'
 require 'pony'
 
+require File.expand_path('../../config/application', __FILE__)
+
 module Application
   class Website < Sinatra::Base
     set :static, true
     set :public_folder, File.expand_path('../../public', __FILE__)
+
+    # Contact form
+    post '/contact' do
+      template = ERB.new(File.read(File.expand_path('../templates/contact.text.erb', __FILE__), :encoding => 'UTF-8'))
+
+      Pony.mail(
+        :from     => params[:email],
+        :to       => COMMAND_EMAIL_TO,
+        :charset  => 'utf-8',
+        :subject  => COMMAND_SUBJECT,
+        :body     => template.result(binding)
+      )
+
+      redirect "/contact.html"
+    end
   end
 end
---
+++ config/application.rb
@@ -0,0 +1,5 @@
+#
+# COMMAND
+#
+COMMAND_EMAIL_TO = 'ndubuis@gmail.com'
+COMMAND_SUBJECT  = 'contact depuis le formulaire du site conte-d-ete.ch'
